19c2738137139070213e65058da0dcb3cf474ca9
itsSopno <sopno263@gmail.com>
2026-06-11 16:30:12 +0600

    Save changes

--- day1.js
+++ day1.js
@@ -24,3 +24,28 @@ function mergeObjects(obj1,obj2){
 }
 
 console.log(mergeObjects({a:1,b:2}, {c:3,d:4}))
+
+// Problem Number 18 
+// FizzBuzz  [Easy]
+// Description: Write a function fizzBuzz(n) that prints numbers from 1 to n. For multiples of 3 print 'Fizz', multiples of 5 print 'Buzz', multiples of both print 'FizzBuzz'.
+// Example:
+// Input: 15Output: 1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz
+// Hint: Check divisibility with the % operator in the right order.
+// Solutions :
+function fizzBuzz(n){
+    for(i=1;i<=n;i++){
+        if(i%3===0&&i%5===0){
+            console.log("FizzBuzz")
+        }else if(i%3===0){
+            console.log("Fizz")
+        }else if(i%5===0){
+            console.log("Buzz")
+        }else{
+            console.log(i)
+        }
+    }
+}
+fizzBuzz(15)
+
+// Problem Number 19 
+// 
