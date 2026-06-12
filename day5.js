d2688281c6c10adf32cf77010a07f4b81b29dd4d
itsSopno <sopno263@gmail.com>
2026-06-13 02:00:22 +0600

    save changes

--- day5.js
+++ day5.js
@@ -14,3 +14,21 @@ function factorial(n){
     }
 }
 console.log(factorial(5))
+
+// Problem number 22 
+// Fibonacci Sequence  [Easy]
+// Description: Write a function fibonacci(n) that returns the nth number in the Fibonacci sequence.
+// Example:
+// Input: 6  → Output: 8 (0,1,1,2,3,5,8...)
+// Hint: Try both iterative and recursive approaches.
+// Solutions :
+ function fibonacci(n){
+    if(n===0){
+        return 0
+    }else if(n===1){
+        return 1
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
+ }
+console.log(fibonacci(6))
