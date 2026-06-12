35fa3f7a2ec2cbf77e8da837f4d3a83448aec631
itsSopno <sopno263@gmail.com>
2026-06-13 03:19:06 +0600

    save changes

--- day5.js
+++ day5.js
@@ -67,14 +67,14 @@ counter.decrement()
 // const add = curry((a,b) => a+b);add(2)(3); // 5
 // Hint: Return a function from inside a function.
 // Solutions:
-// function curry(fn){
-//     return function(a){
-//         return function(b){
-//             return fn(a,b)
-//         }
-//     }
-// }
-// const add = curry((a,b)=>{
-//     return a+b
-// })
-// console.log(add(2)(3))
+function curry(fn){
+    return function(a){
+        return function(b){
+            return fn(a,b)
+        }
+    }
+}
+const add = curry((a,b)=>{
+    return a+b
+})
+console.log(add(2)(3))
