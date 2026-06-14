f6b620fd95736ef6d9aa65e0f45f5ab2d5c690df
itsSopno <sopno263@gmail.com>
2026-06-14 16:02:25 +0600

    save it

--- day6.js
+++ day6.js
@@ -31,3 +31,24 @@ function mergeArrays(...arrays){
 }
 
 console.log(mergeArrays([1,2],[3,4],[5]))
+
+// Problem Number 28 
+// Promise Chain  [Medium]
+// Description: Write a function delay(ms) that returns a Promise that resolves after ms milliseconds. Then chain two delays: first 1 second, then 2 seconds, logging a message after each.
+// Example:
+// delay(1000).then(() => { console.log('1 sec'); return delay(2000); }).then(() => console.log('3 sec total'));
+// Hint: Use new Promise with setTimeout inside.
+// Solutions :
+function delay(mc) {
+    return new Promise((resolve) => {
+        setTimeout(resolve,mc)
+    })
+}
+delay(1000).then(() => {
+    console.log("1 sec")
+    return delay(2000);
+}).then(() => {
+    console.log("3 sec total")
+})
+
+
