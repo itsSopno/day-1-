5813c5f8263b4cb7305d5fe52b82f05a7af81d11
itsSopno <sopno263@gmail.com>
2026-06-17 15:30:49 +0600

    save changes

--- day7.js
+++ day7.js
@@ -126,3 +126,20 @@ emitter.on('greet', greet);
 emitter.emit('greet', 'Sara');
 emitter.off('greet', greet);
 emitter.emit('greet', 'Sara'); 
+
+// Problem Number 35  
+// Implement Array.prototype.map from Scratch  [Medium]
+// Description: Write a function myMap(arr, callback) that replicates the behavior of Array.prototype.map without using the built-in map().
+// Example:
+// myMap([1,2,3], x => x * 2);// Output: [2, 4, 6]
+// Hint: Loop through the array, apply the callback to each element, push results to a new array.
+
+// Solutions : 
+function myMap(arr, callback){
+    const results = [];
+    for(let i = 0; i < arr.length; i++){
+        results.push(callback(arr[i], i, arr))
+    }
+    return results
+}
+console.log(myMap([1,2,3], x => x * 2))
