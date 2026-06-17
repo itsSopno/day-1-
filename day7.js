c796f367e1340655b3d170b1eb3197260ba8cb3b
itsSopno <sopno263@gmail.com>
2026-06-17 15:25:45 +0600

    save changes

--- day7.js
+++ day7.js
@@ -89,3 +89,40 @@ function deepClone(obj){
     return clone;
 }
 
+// Problem Number 34 
+//  Event Emitter  [Medium]
+// Description: Build a simple EventEmitter class with on(event, listener), emit(event, ...args), and off(event, listener) methods.
+// Example:
+// const emitter = new EventEmitter();emitter.on('greet', name => console.log('Hello ' + name));emitter.emit('greet', 'Sara'); // Hello Sara
+// Hint: Store listeners in an object where keys are event names and values are arrays of functions.
+
+// Solutions : 
+class EventEmiter{
+    constructor(){
+        this.events = {}
+    }
+    on(event, listener){
+        if(!this.events[event]){
+            this.events[event] = []
+        }
+        this.events[event].push(listener)
+    }
+    emit(event, ...args){
+        if(this.events[event]){
+            this.events[event].forEach(listener => {
+                listener(...args)
+            })
+        }
+    }
+    off(event, listener){
+        if(this.events[event]){
+            this.events[event] = this.events[event].filter(l => l !== listener)
+        }
+    }
+}
+const emitter = new EventEmiter();
+const greet = (name) => console.log('Hello ' + name);
+emitter.on('greet', greet);
+emitter.emit('greet', 'Sara');
+emitter.off('greet', greet);
+emitter.emit('greet', 'Sara'); 
