4c8eec5f1776d4019aa7aca1d0f930eef347e2b1
itsSopno <sopno263@gmail.com>
2026-06-21 23:16:25 +0600

    save changes

--- day9.js
+++ day9.js
@@ -22,3 +22,21 @@ function twoSum(arr, target){
 const nums = [2, 7, 11, 15]
 const target = 9
 console.log("Problem 41 Result:", twoSum(nums, target))
+
+// Problem Number 42 
+//  Check Anagram  [Easy]
+// Description: Write a function isAnagram(str1, str2) that returns true if the two strings are anagrams of each other.
+// Example:
+// Input: 'listen', 'silent'  → Output: trueInput: 'hello', 'world'   → Output: false
+// Hint: Sort both strings and compare, or use a character frequency map.
+// Solutions : 
+function isAnagram(str1, str2){
+    const sortedStr1 = str1.split('').sort().join('')
+    const sortedStr2 = str2.split('').sort().join('')
+    return sortedStr1 === sortedStr2
+}
+
+// Test 
+const str1 = 'listen'
+const str2 = 'silent'
+console.log("Problem 42 Result:", isAnagram(str1, str2))
